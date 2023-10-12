a24f076917de973183a7cb539c4867ffc6849889
channingbabb <channingbabb@gmail.com>
2023-10-11 21:41:37 -0500

    init

---
+++ src/ChanningBabb_Assignment_ExceptionHandling/SquareOutOfBounds.java
@@ -0,0 +1,8 @@
+package ChanningBabb_Assignment_ExceptionHandling;
+
+public class SquareOutOfBounds extends Exception {
+
+        public SquareOutOfBounds(String message) {
+            super(message);
+        }
+}
